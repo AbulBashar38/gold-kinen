c9c688af3fe3453083d541c76dc6f39097794f79
basar <abulbasarofficial5403@gmail.com>
2025-11-05 18:21:26 +0600

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "https://ygwr7rrpjj.execute-api.ap-southeast-1.amazonaws.com/Stage",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
